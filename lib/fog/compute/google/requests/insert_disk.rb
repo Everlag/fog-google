55f400d69f042ae0b3bd48825358072a95f9c38a
Kyle Boutette <boutette@google.com>
2017-07-20 13:47:15 -0700

    checkpoint

--- lib/fog/compute/google/requests/insert_disk.rb
+++ lib/fog/compute/google/requests/insert_disk.rb
@@ -9,11 +9,33 @@ module Fog
 
       class Real
         def insert_disk(disk_name, zone_name, image_name = nil, opts = {})
+          # According to Google docs, if image name is not present, only one of
+          # sizeGb or sourceSnapshot need to be present, one will create blank
+          # disk of desired size, other will create disk from snapshot
+          if image_name.nil?
+            if opts.key?("sourceSnapshot")
+              # New disk from snapshot
+              snap = snapshots.get(opts.delete("sourceSnapshot"))
+              raise ArgumentError.new("Invalid source snapshot") unless snap
+              body_object["sourceSnapshot"] = @api_url + snap.resource_url
+            elsif opts.key?("sizeGb")
+              # New blank disk
+              body_object["sizeGb"] = opts.delete("sizeGb")
+            else
+              raise ArgumentError.new("Must specify image OR snapshot OR "\
+                                      "disk size when creating a disk.")
+            end
+          end
+
           disk = ::Google::Apis::ComputeV1::Disk.new(
-            :name => address_name,
-            :description => options[:description]
+            :name => disk_name,
+            :description => options["description"],
+            :type => options["type"],
+            :size_gb => options["sizeGb"],
+            source_snapshot => options["sourceSnapshot"]
           )
-          @compute.insert_disk(@project, zone_name, )
+          @compute.insert_disk(@project, zone_name, disk,
+                               :source_image => image_name)
 
           api_method = @compute.disks.insert
           parameters = {
@@ -31,25 +53,6 @@ module Fog
 
           body_object = { "name" => disk_name }
           body_object["type"] = opts.delete("type")
-
-          # According to Google docs, if image name is not present, only one of
-          # sizeGb or sourceSnapshot need to be present, one will create blank
-          # disk of desired size, other will create disk from snapshot
-          if image_name.nil?
-            if opts.key?("sourceSnapshot")
-              # New disk from snapshot
-              snap = snapshots.get(opts.delete("sourceSnapshot"))
-              raise ArgumentError.new("Invalid source snapshot") unless snap
-              body_object["sourceSnapshot"] = @api_url + snap.resource_url
-            elsif opts.key?("sizeGb")
-              # New blank disk
-              body_object["sizeGb"] = opts.delete("sizeGb")
-            else
-              raise ArgumentError.new("Must specify image OR snapshot OR "\
-                                      "disk size when creating a disk.")
-            end
-
-          end
 
           # Merge in any remaining options (only 'description' should remain)
           body_object.merge!(opts)
